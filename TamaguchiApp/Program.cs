2447ceea9c095138b579af219520ec1d5362f559
chenny shahir <chenny.shahir@gmail.com>
2021-01-25 15:27:54 +0200

    OIGJFDFJFSDJFOPJGIJFGIOJFIGJFIOGJIFGJOIFJGOIFJGIOFJHOIJPH

--- TamaguchiApp/Program.cs
+++ TamaguchiApp/Program.cs
@@ -6,7 +6,7 @@ namespace TamaguchiApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("NOIVFDMGFMGOIFJGOIGFJGIKFMGOPFDLDMFOIPRMGOIKNGJOIKFDNGOIDNGOIIFNGIDNGUDBVFIDBGUDRHGURDGH");
         }
     }
 }
